98ad3097aa94fac65d5267b9f0088058ce02612e
TomFrost <tom@frosteddesign.com>
2015-07-02 01:52:12 -0500

    CoreMods: Lint NickServ authmethod

--- app/coremods/users/authmethods/NickServ.js
+++ app/coremods/users/authmethods/NickServ.js
@@ -3,65 +3,73 @@
  * Copyright 2015 Tom Shawver
  */
 
-// Dependencies
-var objUtil = require('../../../util/Object');
+var _ = require('lodash');
 
 const NICKSERV = 'NickServ';
+const REQ_TIMEOUT = 10000;
 const SWITCH_REGEX = /^Unknown command/;
 const TYPES = {
-	// The 'ACC' command is used by Atheme's NickServ
-	ACC: {
-		cmd: 'ACC {nick}',
-		regex: /(?:^|\s)(\S+) ACC (\d)/,
-		success: 3
-	},
-	// The 'STATUS' command is used by Anope's NickServ
-	STATUS: {
-		cmd: 'STATUS {nick}',
-		regex: /^STATUS (\S+) (\d)$/,
-		success: 3
-	}
+  // The 'ACC' command is used by Atheme's NickServ
+  ACC: {
+    cmd: 'ACC {nick}',
+    regex: /(?:^|\s)(\S+) ACC (\d)/,
+    success: 3
+  },
+  // The 'STATUS' command is used by Anope's NickServ
+  STATUS: {
+    cmd: 'STATUS {nick}',
+    regex: /^STATUS (\S+) (\d)$/,
+    success: 3
+  }
 };
 
 module.exports = function(config, client) {
+  var pendingNicks = {};
+  var type = 'ACC';
+  var locked = false;
 
-	var pendingNicks = {},
-		type = 'ACC',
-		locked = false;
+  function handleNotice(from, to, text) {
+    if (!locked && text.match(SWITCH_REGEX)) {
+      type = 'STATUS';
+      locked = true;
+      Object.keys(pendingNicks).forEach(sendCommand);
+    } else if (from && from.toLowerCase() === NICKSERV.toLowerCase()) {
+      var res = text.match(TYPES[type].regex);
+      if (res) {
+        res[1] = res[1].toLowerCase();
+        if (pendingNicks[res[1]]) {
+          locked = true;
+          pendingNicks[res[1]].resolve(res[2] === TYPES[type].success);
+          clearTimeout(pendingNicks.timeout);
+          delete pendingNicks[res[1]];
+        }
+      }
+    }
+  }
+  client.on('notice', handleNotice);
 
-	function handleNotice(from, to, text, message) {
-		if (!locked && text.match(SWITCH_REGEX)) {
-			type = 'STATUS';
-			locked = true;
-			objUtil.forEach(pendingNicks, function(nick, cb) {
-				isAuthorized(nick, null, cb);
-			});
-		}
-		else if (from && from.toLowerCase() == NICKSERV.toLowerCase()) {
-			var res = text.match(TYPES[type].regex);
-			if (res) {
-				res[1] = res[1].toLowerCase();
-				if (pendingNicks[res[1]]) {
-					locked = true;
-					pendingNicks[res[1]](null, res[2] == TYPES[type].success);
-					delete pendingNicks[res[1]];
-				}
-			}
-		}
-	}
-	client.on('notice', handleNotice);
+  function isAuthorized(nick) {
+    nick = nick.toLowerCase();
+    return new Promise(function(resolve, reject) {
+      pendingNicks[nick] = {
+        resolve: resolve,
+        timeout: setTimeout(function() {
+          reject(new Error('NickServ authentication timed out'));
+        }, REQ_TIMEOUT)
+      };
+      sendCommand(nick);
+    });
+  }
 
-	function isAuthorized(nick, opts, cb) {
-		nick = nick.toLowerCase();
-		pendingNicks[nick] = cb;
-		var cmd = TYPES[type].cmd.replace('{nick}', nick);
-		client.say(NICKSERV, cmd);
-	}
+  function sendCommand(nick) {
+    var cmd = TYPES[type].cmd.replace('{nick}', nick);
+    client.say(NICKSERV, cmd);
+  }
 
-	return {
-		isAuthorized: isAuthorized,
-		unload: function() {
-			client.removeListener('notice', handleNotice);
-		}
-	};
+  return {
+    isAuthorized: isAuthorized,
+    unload: function() {
+      client.removeListener('notice', handleNotice);
+    }
+  };
 };
